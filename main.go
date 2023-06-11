f315455f3e6ad7e2e279dfb9cc96063194cd62fc
Alexey Khit <alexey.khit@gmail.com>
2023-06-11 07:34:02 +0300

    Add time to app exit message

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 )
 
 func main() {
@@ -29,5 +30,10 @@ func main() {
 
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-	fmt.Printf("exit with signal: %s\n", <-sigs)
+	log("exit with signal: %s\n", <-sigs)
+}
+
+func log(format string, a ...any) {
+	fmt.Print(time.Now().Format(time.RFC3339), " ")
+	fmt.Printf(format, a...)
 }
